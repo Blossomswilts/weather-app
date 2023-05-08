08563c95d63131edafb05ecb12879e6b0877138a
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-05-07 20:52:28 -0400

    adjusted get local placement

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,12 @@
 //_________________________________Global-Variables_______________________________________________
 const buttonEl = document.querySelector(".btn");
 
+//_________________________________Local-Call_____________________________________________________
+getLocal()
+
+
+
+
 //________________________________Event-Listener__________________________________________________
 // This function will hold the fetch requests for the weather data for current and future days.
 buttonEl.addEventListener("click", function (event) {
@@ -47,11 +53,11 @@ buttonEl.addEventListener("click", function (event) {
                             // turn temp data from kelvin to farenheit
                             let currentTempF = (currentTemp - 273.15) * 1.80 + 32;
                             // add temp data to current weather div
-                            currentTempEl.textContent = "Temperature: " + currentTempF.toFixed(2) + "°F";
+                            currentTempEl.textContent = "Temperature: " + currentTempF.toFixed(0) + "°F";
                             // get icon data and add to icon-current div
                             let currentIcon = data.weather[0].icon;
                             const currentIconEl = document.querySelector(".icon-current");
-                            currentIconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + currentIcon + ".png");
+                            currentIconEl.setAttribute("src", "http://openweathermap.org/img/w/" + currentIcon + ".png");
                         });
                     }
                 });
@@ -106,49 +112,52 @@ buttonEl.addEventListener("click", function (event) {
                             let day5Temp = data.list[4].main.temp;
                             let day5TempF = (day5Temp - 273.15) * 1.80 + 32;
                             //add temp data to the temp divs
-                            day1TempEl.textContent = "Temp: " + day1TempF.toFixed(2) + "°F";
-                            day2TempEl.textContent = "Temp: " + day2TempF.toFixed(2) + "°F";
-                            day3TempEl.textContent = "Temp: " + day3TempF.toFixed(2) + "°F";
-                            day4TempEl.textContent = "Temp: " + day4TempF.toFixed(2) + "°F";
-                            day5TempEl.textContent = "Temp: " + day5TempF.toFixed(2) + "°F";
+                            day1TempEl.textContent = "Temp: " + day1TempF.toFixed(0) + "°F";
+                            day2TempEl.textContent = "Temp: " + day2TempF.toFixed(0) + "°F";
+                            day3TempEl.textContent = "Temp: " + day3TempF.toFixed(0) + "°F";
+                            day4TempEl.textContent = "Temp: " + day4TempF.toFixed(0) + "°F";
+                            day5TempEl.textContent = "Temp: " + day5TempF.toFixed(0) + "°F";
 
                             // get icon data and add to icon divs icon-day1, icon-day2, icon-day3, icon-day4, icon-day5
                             let day1Icon = data.list[0].weather[0].icon;
                             const day1IconEl = document.querySelector(".icon-day1");
-                            day1IconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + day1Icon + ".png");
+                            day1IconEl.setAttribute("src", "http://openweathermap.org/img/w/" + day1Icon + ".png");
                             let day2Icon = data.list[1].weather[0].icon;
                             const day2IconEl = document.querySelector(".icon-day2");
-                            day2IconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + day2Icon + ".png");
+                            day2IconEl.setAttribute("src", "http://openweathermap.org/img/w/" + day2Icon + ".png");
                             let day3Icon = data.list[2].weather[0].icon;
                             const day3IconEl = document.querySelector(".icon-day3");
-                            day3IconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + day3Icon + ".png");
+                            day3IconEl.setAttribute("src", "http://openweathermap.org/img/w/" + day3Icon + ".png");
                             let day4Icon = data.list[3].weather[0].icon;
                             const day4IconEl = document.querySelector(".icon-day4");
-                            day4IconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + day4Icon + ".png");
+                            day4IconEl.setAttribute("src", "http://openweathermap.org/img/w/" + day4Icon + ".png");
                             let day5Icon = data.list[4].weather[0].icon;
                             const day5IconEl = document.querySelector(".icon-day5");
-                            day5IconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + day5Icon + ".png");
+                            day5IconEl.setAttribute("src", "http://openweathermap.org/img/w/" + day5Icon + ".png");
 
 
- //____________________________________local-storage_______________________________________________________
-                            //set cityName as an empty array
-                            let cityName = { inputValue };
-                            //set cityName to local storage from the input value
-                            localStorage.setItem("cityName", JSON.stringify(cityName));
-                            //get cityName from local storage and set it to list
-                            let cityList = JSON.parse(localStorage.getItem("cityName"));
-                            const searchHistoryEl = document.querySelector(".search-history");
-                            const cityListItem = document.createElement("li");
-                            cityListItem.textContent = cityList.inputValue;
-                            searchHistoryEl.appendChild(cityListItem);
-
+ //____________________________________local-storage___________________________________________________________
+                                //set cityName as an empty array
+                                let cityName = { inputValue };
+                                
+                                //set cityName to local storage from the input value
+                                localStorage.setItem("cityName", JSON.stringify(cityName));
+                                cityName.push(inputValue.value);
                         });
                     }
                 });
-
-
             });
         }
     });
 });
 
+
+//____________________________________local-Get_______________________________________________________________
+function getLocal() {
+    //get cityName from local storage and set it to list
+    let cityList = JSON.parse(localStorage.getItem("cityName")) || [];
+    const searchHistoryEl = document.querySelector(".search-history");
+    const cityListItem = document.createElement("li");
+    cityListItem.textContent = cityList.inputValue;
+    searchHistoryEl.appendChild(cityListItem);
+};
